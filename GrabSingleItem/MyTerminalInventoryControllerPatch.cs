2a2cba46e087ee4cee25366ea5e43088513636a7
pas2704 <pas2704@outlook.com>
2024-05-14 11:50:52 -0400

    Initial Commit

---
+++ GrabSingleItem/MyTerminalInventoryControllerPatch.cs
@@ -0,0 +1,36 @@
+﻿using HarmonyLib;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using VRage.Input;
+
+namespace GrabSingleItem
+{
+    [HarmonyPatch]
+    internal static class MyTerminalInventoryControllerPatch
+    {
+        [HarmonyPatch("Sandbox.Game.Gui.MyTerminalInventoryController", "grid_ItemClicked")]
+        [HarmonyTranspiler]
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            foreach (var instruction in instructions)
+            {
+                yield return instruction;
+                if (instruction.opcode == OpCodes.Or)
+                {
+                    yield return CodeInstruction.Call(typeof(MyTerminalInventoryControllerPatch), nameof(CheckForAlt));
+                }
+            }
+        }
+
+        private static bool CheckForAlt(bool original) => original || MyInput.Static.IsAnyAltKeyPressed();
+
+        [HarmonyPatch("Sandbox.Game.Gui.MyTerminalInventoryController", "grid_ItemDragged")]
+        [HarmonyPrefix]
+        private static bool DragPrefix() => !MyInput.Static.IsAnyAltKeyPressed();
+
+        [HarmonyPatch("Sandbox.Game.Gui.MyTerminalInventoryController", "grid_ItemDoubleClicked")]
+        [HarmonyPrefix]
+        private static bool DoubleClickPrefix() => !MyInput.Static.IsAnyAltKeyPressed();
+
+    }
+}
---
+++ GrabSingleItem/Plugin.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using NLog;
+using System.Reflection;
+using VRage.Plugins;
+
+namespace GrabSingleItem
+{
+    public class Plugin : IPlugin
+    {
+        internal static readonly Logger Log = LogManager.GetCurrentClassLogger();
+        public void Init(object gameInstance)
+        {
+            Log.Debug("GrabSingleItem: Patching");
+            new Harmony("GrabSingleItem").PatchAll(Assembly.GetExecutingAssembly());
+            Log.Info("GrabSingleItem: Patches applied");
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public void Update()
+        {
+        }
+    }
+}
